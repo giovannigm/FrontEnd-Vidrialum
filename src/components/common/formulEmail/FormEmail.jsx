732fed0db7342a9df659f5b233bd362062df8d71
giovannigm <giovanniantonygarcia@gmail.com>
2025-06-17 21:53:28 -0300

    Mejora el manejo de errores en el servicio de envío de correos, permite adjuntar archivos en el formulario de contacto y ajusta estilos para una mejor presentación.

--- src/components/common/formulEmail/FormEmail.jsx
+++ src/components/common/formulEmail/FormEmail.jsx
@@ -1,11 +1,25 @@
 import React, { useRef, useState } from "react";
 import "./FormEmail.scss";
 import { emailService } from "../../../Api/axiosInstance";
+import { FaTrash } from "react-icons/fa";
 
 const FormEmail = () => {
   const form = useRef();
   const [submitText, setSubmitText] = useState("enviar");
   const [errorMessage, setErrorMessage] = useState("");
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    setSelectedFile(file);
+  };
+
+  const removeFile = () => {
+    setSelectedFile(null);
+    if (form.current) {
+      form.current.file.value = "";
+    }
+  };
 
   const sendEmail = async (e) => {
     e.preventDefault();
@@ -22,6 +36,9 @@ const FormEmail = () => {
 
     // Crear FormData correctamente
     const formData = new FormData(form.current);
+    if (!selectedFile) {
+      formData.delete("file");
+    }
 
     try {
       // Enviar el FormData al backend
@@ -32,18 +49,22 @@ const FormEmail = () => {
         setTimeout(() => {
           setSubmitText("enviar");
           form.current.reset();
+          setSelectedFile(null);
         }, 2000);
       } else {
-        console.error("Response status:", response.status);
+        console.log("Response:", response.data);
         setSubmitText("enviar");
-        setErrorMessage(
-          "No se pudo enviar el mensaje. Por favor inténtalo de nuevo."
-        );
+        setErrorMessage(`${response.data}`);
       }
     } catch (error) {
-      console.error("Error:", error);
+      if (error.status === 400) {
+        setErrorMessage(error.response.data);
+      }
+      if (error.status >= 500) {
+        setErrorMessage(error.response.data?.message);
+      }
+      console.log("Error:", error);
       setSubmitText("enviar");
-      setErrorMessage("Se produjo un error. Por favor inténtalo de nuevo.");
     }
   };
 
@@ -53,6 +74,7 @@ const FormEmail = () => {
       onSubmit={sendEmail}
       className="field"
       encType="multipart/form-data"
+      style={{ background: "none", boxShadow: "none" }}
     >
       <input
         className="infoContact"
@@ -69,7 +91,44 @@ const FormEmail = () => {
         required
       />
       <textarea name="message" placeholder="Ingrese su mensaje" required />
-      <input className="fileClass" type="file" name="file" />
+
+      <label className="fileClass">
+        Adjuntar archivos 📎
+        <input
+          type="file"
+          name="file"
+          style={{ display: "none" }}
+          onChange={handleFileChange}
+        />
+      </label>
+
+      {selectedFile && (
+        <div style={{ width: "40%", alignItems: "center" }}>
+          <span className="file-chip__icon">✔️</span>
+          <span
+            className="file-chip__name"
+            style={{
+              display: "inline-block",
+              maxWidth: "150px",
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+              whiteSpace: "nowrap",
+              verticalAlign: "middle",
+            }}
+            title={selectedFile.name}
+          >
+            {selectedFile.name}
+          </span>
+          <button
+            type="button"
+            style={{ border: "none", background: "none", fontSize: "1rem" }}
+            onClick={removeFile}
+          >
+            <FaTrash />
+          </button>
+        </div>
+      )}
+
       {errorMessage && <p className="error-message">{errorMessage}</p>}
       <input id="btSub" type="submit" value={submitText} />
     </form>
